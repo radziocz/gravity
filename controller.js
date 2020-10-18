5ab5bd08e706548cad79b25750879f4a274c6e3a
radek <radek@radoslaw.waw.pl>
2020-10-18 03:18:40 +0200

    safty first

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ exports.home = (req, res) => {
     res.render('form')
 }
 
-exports.run = (req, res) => {
+exports.run = (req, res, next) => {
     res.render('space', {
         "mass_x": req.body.mass_x,
         "mass_y": req.body.mass_y,
@@ -13,4 +13,14 @@ exports.run = (req, res) => {
         "x_speed_vector": req.body.x_speed_vector,
         "y_speed_vector": req.body.y_speed_vector
     })
+    return next();
 }
+
+exports.validate = [
+    check('mass_x').isNumeric().escape().withMessage('Number is required'),
+    check('mass_y').isNumeric().escape().withMessage('Number is required'),
+    check('x').isNumeric().escape().withMessage('Number is required'),
+    check('y').isNumeric().escape().withMessage('Number is required'),
+    check('x_speed_vector').isNumeric().escape().withMessage('Number is required'),
+    check('y_speed_vector').isNumeric().escape().withMessage('Number is required')
+];
--- routing.js
+++ routing.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controller = require('./controller')
 
 router.get('/', controller.home)
-router.post('/run', controller.run)
+router.post('/run', controller.validate,
+                    controller.run)
 
 module.exports = router
